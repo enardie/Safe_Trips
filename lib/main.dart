7e8fc01f21967d3ae3a72e7026461bfe9c436ce1
Katiuscka van Veen <katiuscka@hotmail.com>
2021-03-07 12:00:53 +0100

    more things

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,97 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: Text("Bon Voyage"), centerTitle: true),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                      child: ClipRRect(
-                    child: Image.asset(
-                      'images/mdma.jpg',
-                    ),
-                  )),
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Drug App', home: MainPage());
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Bon Voyage"), centerTitle: true),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            InkWell(
+              onTap: () {
+                navigateToSubPage(context);
+              },
+              child: Container(
+                  child: ClipRRect(
+                child: Image.asset(
+                  'images/mdma.jpg',
                 ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                      child: ClipRRect(
-                    child: Image.asset(
-                      'images/alcohol.jpg',
-                    ),
-                  )),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                      child: ClipRRect(
-                    child: Image.asset(
-                      'images/cocaine.jpg',
-                    ),
-                  )),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                      child: ClipRRect(
-                    child: Image.asset(
-                      'images/heroin.jpg',
-                    ),
-                  )),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                      child: ClipRRect(
-                    child: Image.asset(
-                      'images/marijuana.jpg',
-                    ),
-                  )),
-                ),
-              ],
+              )),
             ),
-          ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                  child: ClipRRect(
+                child: Image.asset(
+                  'images/alcohol.jpg',
+                ),
+              )),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                  child: ClipRRect(
+                child: Image.asset(
+                  'images/cocaine.jpg',
+                ),
+              )),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                  child: ClipRRect(
+                child: Image.asset(
+                  'images/heroin.jpg',
+                ),
+              )),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                  child: ClipRRect(
+                child: Image.asset(
+                  'images/marijuana.jpg',
+                ),
+              )),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  Future navigateToSubPage(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => SubPage()));
+  }
+}
+
+class SubPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Bon Voyage"), centerTitle: true),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            InkWell(
+              onTap: () {},
+              child: Container(
+                  child: ClipRRect(
+                      child: Image.asset('images/mdma-structure.jpg'))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

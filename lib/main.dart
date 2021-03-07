841411369243cf64bd061859cf77e12b748cbe75
Katiuscka van Veen <katiuscka@hotmail.com>
2021-03-07 10:51:02 +0100

    scrolls

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 
-
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Bon Voyage"),
-      centerTitle: true
-    ),
-    body: Center(
-      child: Image.asset('images/mdma.jpg'),
-
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text("Bon Voyage"), centerTitle: true),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset('images/mdma.jpg'),
+                Image.asset('images/alcohol.jpg'),
+                Image.asset('images/cocaine.jpg'),
+                Image.asset('images/heroin.jpg'),
+                Image.asset('images/marijuana.jpg'),
+              ],
+            ),
+          ),
+        ),
       ),
-      ),
-
-
-    ),
-
-);
-
-
+    );
